97e374f41fae5e06ae61b1cf88b74d8560438326
koponirov <koponirov92@gmail.com>
2021-03-03 12:18:51 +0700

    gh

--- index.js
+++ index.js
@@ -19,12 +19,9 @@ let container = document.getElementById('#container')
 console.log(container)
 
 streams.forEach((s, i)=> {
+
     let srcPath = `https://cam.mega-com.ru/site/embed.html?id=${s.id}&html5=${html5}&logo=${logo}&w=${width}&h=${window.innerHeight/2}&ids=${seeAlsoCameraIDList && seeAlsoCameraIDList.length ? JSON.stringify(seeAlsoCameraIDList) : ''}&autostart=true`
-
     let style = `width: ${width}px; height: ${(window.innerHeight/2)-40}px; border: 0px;`
-    //let srcPath1 = "https://cam.mega-com.ru/site/embed.html?id=3941&html5=true&logo='&w=1140&h=641&ids=&autostart=true"
-
-    //let stream = document.createElement(`<iframe id=${id} scrolling="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>`)
 
     container.insertAdjacentHTML('beforeend', `
         <div class="item">
@@ -34,13 +31,8 @@ streams.forEach((s, i)=> {
             <span class="subtitle">${s.address}</span>
         </div>`)
 
-    // document.write(`<iframe id=${id} scrolling="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe>`);
-    //
     const stream = document.getElementById(`${s.id+i}`)
     stream.id = s.id
     stream.src = srcPath
     stream.style = style
-
-
-
 })
